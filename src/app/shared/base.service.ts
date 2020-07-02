df32edfeaf417eb3b0cabb796293652ca8b1149c
Jbohn44 <ejbohnert@yahoo.com>
2020-07-01 19:55:15 -0500

    work to http base service

--- src/app/shared/base.service.ts
+++ src/app/shared/base.service.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService {
+  baseUrl = "this/base/url";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  get(){
-
-  }
-  post(){
-
-  }
-  put(){
+  get(url){
+    return this.http.get(this.baseUrl + url);
 
   }
-  delete(){
-    
+  post(url, params: any[]){
+    return this.http.post(this.baseUrl + url, params);
+  }
+  put(url, params: any[]){
+    return this.http.put(this.baseUrl + url, params);
+  }
+  delete(url){
+    return this.http.delete(this.baseUrl + url);
   }
 }
