6c85adb192d5d1e4f978a5932457b67d51ed06f6
Jbohn44 <ejbohnert@yahoo.com>
2020-07-03 19:38:23 -0500

    work with sliders css

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -22,15 +22,15 @@ export const apiUrls = {
         RatingType: "OverAllRating"
       },
       FlavorRating: <Rating>{
-        RatingValue: 4,
+        RatingValue: 5,
         RatingType: "FlavorRating"
       },
       MouthFeelRating: <Rating>{
-        RatingValue: 1,
+        RatingValue: 4,
         RatingType: "MouthFeelRating"
       },
       DensityRating: <Rating>{ 
-        RatingValue: 2,
+        RatingValue: 3,
         RatingType: "DensityRating"
       },
       Comments: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
