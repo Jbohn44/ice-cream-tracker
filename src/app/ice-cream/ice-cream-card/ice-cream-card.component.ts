473c798b4d35780f39a96aaff0fccd93efa81041
Jbohn44 <ejbohnert@yahoo.com>
2020-07-04 15:52:46 -0500

    changed display of ice cream cards

--- src/app/ice-cream/ice-cream-card/ice-cream-card.component.ts
+++ src/app/ice-cream/ice-cream-card/ice-cream-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IceCream } from '../models/ice-cream.model';
-import { RatingTypes } from '../../shared/constants';
+import { RatingTypes, MouthFeelEnum, FlavorEnum, DensityEnum } from '../../shared/constants';
 
 @Component({
   selector: 'app-ice-cream-card',
@@ -10,6 +10,9 @@ import { RatingTypes } from '../../shared/constants';
 export class IceCreamCardComponent implements OnInit {
   @Input() iceCream: IceCream;
   ratingTypes = RatingTypes.RatingTypes;
+  flavorEnum = FlavorEnum;
+  mouthFeelEnum = MouthFeelEnum;
+  densityEnum = DensityEnum;
   // this is an input into the rating component to disallow editing of ratings on card.  editing will be done it edit section
   edit = false;
   constructor() { }
